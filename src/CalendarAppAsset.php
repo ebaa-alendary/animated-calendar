7af575ac30bde788b31713b025ffa91da17b2a79
ebaa_alendary <ebaa.m.alendary22@gmail.com>
2022-03-27 21:02:02 +0200

    -

--- src/CalendarAppAsset.php
+++ src/CalendarAppAsset.php
@@ -1,21 +1,22 @@
 <?php
 
+namespace AnimatedCalendar\Yii2AnimatedCalendar;
 
 use yii\web\AssetBundle;
 
 class CalendarAppAsset extends AssetBundle
 {
-    public $basePath = '@app/vendor/animated-calendary/yii2-animated-calendary';
-    public $baseUrl = '@app/vendor/animated-calendary/yii2-animated-calendary';
+    public $sourcePath = '@app/vendor/animated-calendary/yii2-animated-calendary/src';
+
     public $css = [
         'src/css/calendar-gc.css',
     ];
     public $js = [
         'src/js/calendar-gc.js',
     ];
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap4\BootstrapAsset',
-    ];
+//    public $depends = [
+//        'yii\web\YiiAsset',
+//        'yii\bootstrap4\BootstrapAsset',
+//    ];
 
 }
