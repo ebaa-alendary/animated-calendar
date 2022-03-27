7a01feca500826137f8e5e39d6a7a0b55adb3e6c
ebaa_alendary <ebaa.m.alendary22@gmail.com>
2022-03-27 17:12:28 +0200

    init

--- src/CalendarAppAsset.php
+++ src/CalendarAppAsset.php
@@ -1,11 +1,21 @@
 <?php
 
 
-class CalendarAppAsset
+use yii\web\AssetBundle;
+
+class CalendarAppAsset extends AssetBundle
 {
-    public static function Hello()
-    {
-        return 'Hello!';
-    }
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'src/css/calendar-gc.css',
+    ];
+    public $js = [
+        'src/js/calendar-gc.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
 
 }
